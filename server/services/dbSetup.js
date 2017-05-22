67479e0685a05aeadb41ed8bd476d3f6dc3220bb
juliecoding <julie.akohler@gmail.com>
2017-05-22 14:05:39 -0600

    Fixing db glitches

--- server/services/dbSetup.js
+++ server/services/dbSetup.js
@@ -3,22 +3,25 @@ var app = require('./../index');
 var db = app.get('db');
 var config = require('./../config');
 
-// ALLOW CONSOLE OUTPUT //
-var allowConsolePutput = config.INITIALIZE_LOG;
-var log = function(input) {
-	if (allowConsolePutput) {
-		console.log(input);
-	}
-};
+// // ALLOW CONSOLE OUTPUT //
+// var allowConsolePutput = config.INITIALIZE_LOG;
+// var log = function(input) {
+// 	if (allowConsolePutput) {
+// 		console.log(input);
+// 	}
+// };
 
 // INITIALIZE FUNCTION //
 module.exports = {
-	run: function() {
-		console.log('Initializing database');
+  run: function() {
+    console.log('Initializing database');
 
-		db.initialize.tables_initialize(function(err, table) {
-			if (err) return log('Some tables failed to create');
-			else log('Tables successfully initialized');
-		});
-	}
+    db.initialize.tables_initialize(function(err, table) {
+      if (err) {
+        console.log('Some tables failed to create');
+      } else {
+        console.log('Tables successfully initialized');
+      }
+    });
+  }
 };
