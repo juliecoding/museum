85b8621cf3be29884a11b947bbc2f96e96b9ab23
juliecoding <julie.akohler@gmail.com>
2017-05-23 16:13:09 -0600

    Gulped again

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -2,83 +2,82 @@ var app = angular.module("app", ['ui.router', 'angular-stripe'])
 
 .config(["$stateProvider", "$urlRouterProvider", "stripeProvider", function($stateProvider, $urlRouterProvider, stripeProvider) {
 
-	stripeProvider.setPublishableKey('pk_test_NptqejbBDylUODHwNCKeqnD8');
+  stripeProvider.setPublishableKey('pk_test_buXe8Kr3ohMMAv4KnFoTztiu');
 
-	$urlRouterProvider.otherwise("/");
+  $urlRouterProvider.otherwise("/");
 
-	$stateProvider
-		.state('home', {
-			url: "/",
-			templateUrl: "./app/routes/home/homeTmpl.html",
-			controller: 'homeCtrl'
-		})
-		.state('art', {
-			url: "/explore",
-			templateUrl: "./app/routes/art/artTmpl.html",
-			controller: 'artCtrl'
-		})
-		.state('visit', {
-			url: "/visit",
-			templateUrl: "./app/routes/visit/visitTmpl.html",
-			controller: 'visitCtrl'
-		})
-		.state('shop', {
-			url: "/shop",
-			templateUrl: './app/routes/shop/shopTmpl.html',
-			controller: 'shopCtrl'
-		})
-		.state('account', {
-			url: '/account',
-			templateUrl: './app/routes/account/accountTmpl.html',
-			controller: 'accountCtrl'
-		})
-		.state('products', {
-			url: '/products',
-			templateUrl: './app/routes/products/productsTmpl.html',
-			controller: 'productsCtrl'
-		})
-		.state('orders', {
-			url: '/orders',
-			templateUrl: './app/routes/orders/ordersTmpl.html',
-			controller: 'ordersCtrl'
-		})
-		.state('payment', {
-			url: '/payment',
-			templateUrl: './app/routes/payment/payment.html',
-			controller: 'paymentCtrl'
-		})
-		.state('congrats', {
-			url: '/congrats',
-			templateUrl: './app/routes/congrats/congrats.html',
-			controller: 'paymentCtrl'
-		})
-		.state('acknowledgements', {
-			url: '/acknowledgements',
-			templateUrl: './app/routes/acknowledgements/acknowledgementsTmpl.html'
-		})
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "./app/routes/home/homeTmpl.html",
+      controller: 'homeCtrl'
+    })
+    .state('art', {
+      url: "/explore",
+      templateUrl: "./app/routes/art/artTmpl.html",
+      controller: 'artCtrl'
+    })
+    .state('visit', {
+      url: "/visit",
+      templateUrl: "./app/routes/visit/visitTmpl.html",
+      controller: 'visitCtrl'
+    })
+    .state('shop', {
+      url: "/shop",
+      templateUrl: './app/routes/shop/shopTmpl.html',
+      controller: 'shopCtrl'
+    })
+    .state('account', {
+      url: '/account',
+      templateUrl: './app/routes/account/accountTmpl.html',
+      controller: 'accountCtrl'
+    })
+    .state('products', {
+      url: '/products',
+      templateUrl: './app/routes/products/productsTmpl.html',
+      controller: 'productsCtrl'
+    })
+    .state('orders', {
+      url: '/orders',
+      templateUrl: './app/routes/orders/ordersTmpl.html',
+      controller: 'ordersCtrl'
+    })
+    .state('payment', {
+      url: '/payment',
+      templateUrl: './app/routes/payment/payment.html',
+      controller: 'paymentCtrl'
+    })
+    .state('congrats', {
+      url: '/congrats',
+      templateUrl: './app/routes/congrats/congrats.html',
+      controller: 'paymentCtrl'
+    })
+    .state('acknowledgements', {
+      url: '/acknowledgements',
+      templateUrl: './app/routes/acknowledgements/acknowledgementsTmpl.html'
+    })
 
 
-		// .state('profile', {
-		// 	url: '/profile',
-		// 	templateUrl: './app/routes/profile/profileTmpl.html',
-		// 	controller: 'profileCtrl',
-		// 	resolve: {
-		// 		user: function(authService, $state) {
-		// 			return authService.getCurrentUser()
-		// 				.then(function(response) {
-		// 					if (!response.data)
-		// 						$state.go('login');
-		// 					return response.data;
-		// 				})
-		// 				.catch(function(err) {
-		// 					$state.go('login');
-		// 				});
-		// 		}
-		// 	}
-		// });
+  // .state('profile', {
+  // 	url: '/profile',
+  // 	templateUrl: './app/routes/profile/profileTmpl.html',
+  // 	controller: 'profileCtrl',
+  // 	resolve: {
+  // 		user: function(authService, $state) {
+  // 			return authService.getCurrentUser()
+  // 				.then(function(response) {
+  // 					if (!response.data)
+  // 						$state.go('login');
+  // 					return response.data;
+  // 				})
+  // 				.catch(function(err) {
+  // 					$state.go('login');
+  // 				});
+  // 		}
+  // 	}
+  // });
 
 }]);
-
 angular.module("app")
 	.service("authService", ["$http", function($http) {
 
@@ -396,11 +395,6 @@ angular.module("app").controller("artCtrl", ["$scope", function($scope) {
   // ============================================================
 }]);
 
-angular.module("app").controller("homeCtrl", ["$scope", function($scope) {
-  $scope.hello = 'Hello World!';
-  
-}]);
-
 angular.module("app").controller("ordersCtrl", ["$scope", "stripe", "$http", "$state", "orderService", "incartService", function($scope, stripe, $http, $state, orderService, incartService) {
 
   $scope.getTotal = function() {
@@ -480,6 +474,11 @@ angular.module("app").controller("ordersCtrl", ["$scope", "stripe", "$http", "$s
        });
    };
 
+}]);
+
+angular.module("app").controller("homeCtrl", ["$scope", function($scope) {
+  $scope.hello = 'Hello World!';
+  
 }]);
 
 angular.module('app')
